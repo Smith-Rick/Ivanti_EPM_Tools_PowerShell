67b222404fcd7cd8b00830872be27700bf05f8bb
Rick Smith <Smith-Rick@users.noreply.github.com>
2019-10-16 11:33:47 -0700

    Removed comments section
    Removed comment section from script to ensure EPM patch didnt have issues with it.

--- Validate_LDISCNvroot.ps1
+++ Validate_LDISCNvroot.ps1
@@ -3,20 +3,6 @@ exe=powershell.exe
 args=-executionpolicy bypass %filename%
 filename=detect.ps1
 
-<#	
-	.NOTES
-	===========================================================================
-	 Created on:   	10/16/2019 09:54 AM
-	 Created by:   	Smith-Rick
-	 Filename:     	Validate_LDISCNvroot.ps1
-	===========================================================================
-	.DESCRIPTION
-		Validates that the ldiscn.vroot file has a value other than 'LDCLIENTDIRECTORY'.
-
-		Validation need is a result of identifying systems that had an XML file 
-		with a bogus result even with a clean install. 
-#>
-
 echo off
 $path = 'C:\Program Files (x86)\LANDesk\Shared Files\cbaroot\ldiscn.vroot'
 
